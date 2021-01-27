79d7cd8f76fb6d4f23b976ad9a3b8467b66a6eb8
Михаил Литвинов <myulitvinov@edu.hse.ru>
2021-01-27 18:04:09 +0300

    Third (test) commit

---
+++ Module 3/Seminar2_20.01/Homework/HomeWork(3, 4)/Robotics/Robot.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Robotics {
+    public class Robot {
+
+        // -------------------------------
+        // Поля класса: 
+
+
+        /// <summary>
+        /// Приватное поле класса, соответствующее фиксированной высоте рамки экрана. 
+        /// </summary>
+        private uint height;
+        /// <summary>
+        /// Приватное поле класса, соответствующее фиксированной ширине рамки экрана. 
+        /// </summary>
+        private uint width;
+
+        /// <summary>
+        /// Публично-приватное свойство класса, соответствующее изменяемой координате по оси X для исходного Робота.
+        /// </summary>
+        public uint x { get; private set; } = 1;
+        /// <summary>
+        /// Публично-приватное свойство класса, соответствующее изменяемой координате по оси Y для исходного Робота.
+        /// </summary>
+        public uint y { get; private set; } = 1;
+
+
+        // -------------------------------
+        // Методы класса: 
+
+        /// <summary>
+        /// Стандартный конструктор объекта типа "Робот", задающий размеры рамки консоли. 
+        /// </summary>
+        /// <param name="height"> Высота рамки консоли. </param>
+        /// <param name="width"> Ширина рамки консоли. </param>
+        public Robot (uint height, uint width) {
+            this.height = height;
+            this.width = width;
+        }
+
+        /// <summary>
+        /// Вспомогательный метод, реализующий передвижение Робота по координате вправо. 
+        /// </summary>
+        public void Right() {
+            Console.SetCursorPosition((int)x, (int)y);
+            Console.Write("+", ConsoleColor.Gray);
+            ++x;
+            if (x == width + 1) {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// Вспомогательный метод, реализующий передвижение Робота по координате влево.
+        /// </summary>
+        public void Left() {
+            Console.SetCursorPosition((int)x, (int)y);
+            Console.Write("+", ConsoleColor.Gray);
+            --x;
+            if (x == 0) {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// Вспомогательный метод, реализующий передвижение Робота по координате вверх. 
+        /// </summary>
+        public void Forward() {
+            Console.SetCursorPosition((int)x, (int)y);
+            Console.Write("+", ConsoleColor.Gray);
+            --y; 
+            if (y == 0) {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// Вспомогательный метод, реализующий передвижение Робота по координате вниз. 
+        /// </summary>
+        public void Backward() {
+            Console.SetCursorPosition((int)x, (int)y);
+            Console.Write("+", ConsoleColor.Gray);
+            ++y;
+            if (y == height + 1) {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// Вспомогательный метод, отображающий информацию о текущей позиции Робота на плоскости. 
+        /// </summary>
+        /// <returns></returns>
+        public string Position() {
+            return String.Format("координаты x = {0}, y = {1}", x, y);
+        }
+
+        // -------------------------------
+        // -------------------------------
+    }
+}
---
+++ Module 3/Seminar2_20.01/Homework/HomeWork(3, 4)/Task3/StaticTempConverters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3 {
+    static class StaticTempConverters {
+        public static double CelsToFahr(double temperatureToConvert) {
+            Console.Write("Перевод из градусов по Цельсию в градусы по Фаренгейту: ");
+            return ((double)9 / 5) * temperatureToConvert + 32;
+        }
+
+        public static double FahrToCels(double temperatureToConvert) {
+            Console.Write("Перевод из градусов по Фаренгейту в градусы по Цельсию: ");
+            return ((double)5 / 9) * (temperatureToConvert - 32);
+        }
+
+        public static double CelsToKelvin(double temperatureToConvert) {
+            Console.Write("Перевод из градусов по Цельсию в градусы по Кельвину: ");
+            return temperatureToConvert + 273.15;
+        }
+        public static double KelvinToCels (double temperatureToConvert)
+        {
+            Console.Write("Перевод из градусов по Кельвину в градусы по Цельсию: ");
+            return temperatureToConvert - 273.15;
+        }
+        public static double CelsToRankin(double temperatureToConvert)
+        {
+            Console.Write("Перевод из градусов по Цельсию в градусы по Ранкину: ");
+            return (temperatureToConvert + 273.15) * ((double)9/5);
+        }
+        public static double RankinToCels(double temperatureToConvert)
+        {
+            Console.Write("Перевод из градусов по Ранкину в градусы по Цельсию: ");
+            return (temperatureToConvert - 491.67) * ((double)5 / 9);   
+        }
+        public static double CelsToReaumur(double temperatureToConvert)
+        {
+            Console.Write("Перевод из градусов по Цельсию в градусы по Реомюру: ");
+            return temperatureToConvert * ((double)4/5);
+        }
+        public static double ReaumurToCels(double temperatureToConvert)
+        {
+            Console.Write("Перевод из градусов по Реомюру в градусы по Цельсию: ");
+            return temperatureToConvert * ((double)5 / 4);
+        }
+    }
+}
