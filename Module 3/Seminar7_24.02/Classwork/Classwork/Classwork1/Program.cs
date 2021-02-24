8ace22c5b3cf4dfc845de74492dd0df3ffd2e7e6
Михаил Литвинов <myulitvinov@edu.hse.ru>
2021-02-24 21:32:22 +0300

    New commit.

---
+++ Module 3/Seminar7_24.02/Classwork/Classwork/Classwork1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Classwork1
+{
+    struct Coords
+    {
+        public double x;
+        public double y;
+        public Coords(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString() {
+            return $"Coord. x = {this.x}, Coord.y = {this.y}";
+        }
+    }
+
+    class Circle {
+
+        public double Radius { get; }
+
+        public Coords Center { get; }
+
+        public Circle(double x, double y, double radius) {
+            this.Center = new Coords(x, y);
+            this.Radius = radius;
+        }
+
+        public double Square() => Math.PI * Math.Pow(this.Radius, 2);
+
+        public double Length() => Math.PI * 2 * this.Radius;
+
+        public override string ToString() {
+            return $"Center in: ({this.Center.ToString()}), Radius = {this.Radius}, Square = {this.Square()}, Length = {this.Length()}";
+
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine(new Circle(2, 3, 4));
+        }
+    }
+}
---
+++ Module 3/Seminar7_24.02/Classwork/Classwork/Classwork2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Classwork2
+{
+    
+
+    public struct CircleS : IComparable<CircleS>
+    {
+        public struct PointS {
+            public double X { get; }
+            public double Y { get; }
+            public PointS(double x, double y) {
+                this.X = x;
+                this.Y = y;
+            }
+            public static double Distance(PointS firstPoint, PointS secondPoint) =>
+                Math.Sqrt(Math.Pow(firstPoint.X - secondPoint.X, 2) + Math.Pow(firstPoint.Y - secondPoint.Y, 2)); 
+        }
+
+
+        public PointS Center { get; }
+
+        public double Radius { get; }
+
+        public CircleS(double x, double y, double radius)
+        {
+            this.Center = new PointS(x, y);
+            this.Radius = radius;
+        }
+
+        public override string ToString()
+        {
+            return $"Center in: ({this.Center.ToString()}), Radius = {this.Radius}";
+
+        }
+
+        int IComparable<CircleS>.CompareTo(CircleS objectToCompare) {
+            if (this.Radius > objectToCompare.Radius) {
+                return 1;
+            } else if (this.Radius < objectToCompare.Radius) {
+                return -1;
+            } else { return 0; }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main() {
+            Console.WriteLine(new CircleS(3, 4, 3));
+        }
+    }
+}
