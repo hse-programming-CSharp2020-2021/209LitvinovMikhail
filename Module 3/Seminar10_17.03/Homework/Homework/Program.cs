d3914a75bf969bfee407d5f69f466b7242420865
Михаил Литвинов <myulitvinov@edu.hse.ru>
2021-03-23 06:53:32 +0300

    Added homework.

---
+++ Module 3/Seminar10_17.03/Homework/Homework/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using MagicLibrary;
+namespace Homework
+{
+    class Program
+    {
+        enum ReadArrayMethod : int
+        {
+            RandomValues, ReadFromFile
+        }
+
+
+        private const string dataFile = @"data.txt";
+
+        private static ReadArrayMethod ReadingMethod { get; set; }
+
+        private static Street[] StreetsArray { get; set; }
+
+        public static void Main() {
+            int linesCount = 0;
+            try {
+                Street.ValidateFile(Program.dataFile, out linesCount);
+                Console.WriteLine($"Заданный файл полностью удовлетворяет условиям чтения информации {Environment.NewLine}{Environment.NewLine}");
+                Program.ReadingMethod = ReadArrayMethod.ReadFromFile;
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message + $" -{Environment.NewLine} значения будут получения случайным образом{Environment.NewLine}{Environment.NewLine}");
+                Program.ReadingMethod = ReadArrayMethod.RandomValues;
+            }
+            int N;
+            bool flag = false;
+            do {
+                if (flag) {
+                    Console.WriteLine("Введенное значение должно быть целым и больше нуля");
+                }
+                flag = !int.TryParse(Console.ReadLine(), out N) || N <= 0;
+            } while (flag);
+            N = Math.Min(N, linesCount);
+            Program.StreetsArray = new Street[N];
+            using (StreamReader reader = new StreamReader(Program.dataFile)) {
+                for (int i = 0; i < N; ++i) {
+                    Program.StreetsArray[i] = (Program.ReadingMethod == ReadArrayMethod.ReadFromFile) ?
+                        new Street(reader.ReadLine()) :
+                        new Street();
+                }
+            }
+        }
+    }
+}
---
+++ Module 3/Seminar10_17.03/Homework/MagicLibrary/Street.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace MagicLibrary {
+    public class Street {
+
+        public string Name { get; private set; }
+
+        public int[] Houses { get; private set; }
+
+        private static Random RndGen { get; } = new Random();
+
+        public Street() {
+            this.Name = Street.GenerateName();
+            this.Houses = new int[RndGen.Next(2, 10)];
+            for (int i = 0; i < ~this; ++i) {
+                this.Houses[i] = RndGen.Next(1, 101);
+            }
+        }
+
+        public Street (string input) {
+            string[] elements = input.Split(' ');
+            if (elements.Length < 2) { throw new FormatException("Одна из заданных строк задана в некорректном формате."); }
+            string[] houseNumbers = null;
+            elements.CopyTo(houseNumbers, 1);
+            this.Name = elements[0];
+            this.Houses = Array.ConvertAll<string, int>(houseNumbers,
+                delegate (string element) {
+                    if (!int.TryParse(element, out int buf) || buf < 0) {
+                        throw new FormatException("Одна из заданных строк задана в некорректном формате.");
+                    }
+                    return buf;
+                });
+        }
+
+        private static string GenerateName() {
+            StringBuilder builder = new StringBuilder(RndGen.Next(3, 11));
+            builder[0] = char.ToUpper((char)RndGen.Next((int)'a', (int)'z' + 1));
+            for (int i = 1; i < builder.Length; ++i) {
+                builder[i] = (char)RndGen.Next((int)'a', (int)'z' + 1);
+            }
+            return builder.ToString();
+        }
+
+        public static int operator ~(Street street) => street.Houses.Length;
+
+        public static bool operator !(Street street) {
+            foreach (int houseNumber in street.Houses) {
+                if (houseNumber.ToString().Contains(7.ToString())) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override string ToString() => $"Street {this.Name} has {~this} houses :" +
+            string.Join(", ", this.Houses);
+
+        public static bool ValidateFile(string filePath, out int linesCount) {
+            using (StreamReader reader = new StreamReader(filePath)) {
+                if (!File.Exists(filePath)) { throw new FileNotFoundException("Указанного файла не существует."); }
+                string[] lines = File.ReadAllLines(filePath);
+                if (lines.Length == 0) { throw new FormatException("Указанный файл пуст."); }
+                foreach (string line in lines) {
+                    Street tryToCreateStreet = new Street(line);
+                }
+                linesCount = lines.Length;
+                return true;
+            }
+        }
+    }
+}
